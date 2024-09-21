8fef4a520668d64dd2fa7e811e589c6cf1efbd8a
lzw-723 <lzw-723@qq.com>
2024-09-21 22:39:13 +0800

    使用异步任务

--- Program.cs
+++ Program.cs
@@ -13,54 +13,9 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 var app = builder.Build();
 
-
-IDocument doc = await WebParse.GetDoc("https://skb-nyaa.hacgn.eu.org/?q=aliceholic&f=0&c=2_0");
-
-var tableRows = doc.QuerySelectorAll("tr");
-
 var artworksList = new List<Artwork>();
 
-foreach (var tr in tableRows)
-{
-    var titleElement = tr.QuerySelector("tr td:nth-child(2) a:nth-last-child(1)");
-    // 跳过表头
-    if (titleElement == null) continue;
-    var title = titleElement!.TextContent.Trim();
-    var artist = "";
-    // 使用正则表达式查找所有[]内的内容
-    MatchCollection items = Regex.Matches(title, @"\[([^\[\]]*)\]");
-    if (items.Count > 1)
-    {
-        Console.WriteLine($"{title}");
-        // 获取第一个匹配项
-        // title = items[0].Value;
-        // title = title.Substring(1, title.Length - 2);
-        artist = items[1].Value;
-        artist = artist.Substring(1, artist.Length - 2);
-    }
-
-
-    var link = titleElement!.GetAttribute("href")!.Trim();
-
-    var view = await WebParse.GetDoc("https://skb-nyaa.hacgn.eu.org" + link);
-    var desc = view.QuerySelector("#torrent-description")!.InnerHtml;
-
-    string imageUrl = "";
-    // 使用正则表达式查找第一个图片的URL  
-    string pattern = @"!\[[^\]]*\]\(([^)]*)\)";
-    Match match = Regex.Match(desc, pattern);
-    if (match.Success)
-    {
-        // match.Groups[1] 包含了第一个捕获组的内容，即图片的URL  
-        imageUrl = match.Groups[1].Value;
-    }
-
-    var magnetElement = tr.QuerySelector("tr td:nth-child(3) a:nth-child(2)");
-    var magnet = magnetElement!.GetAttribute("href")!.Trim();
-
-    artworksList.Add(new Artwork(title, artist, magnet, imageUrl));
-    Console.WriteLine($"{title} {magnet}");
-}
+FetchNyaa(artworksList);
 
 var api = app.MapGroup("/api/v1");
 var todosApi = api.MapGroup("/artworks");
@@ -75,6 +30,58 @@ app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
 
 app.Run();
+
+async Task FetchNyaa(List<Artwork> artworks)
+{
+    IDocument doc = await WebParse.GetDoc("https://skb-nyaa.hacgn.eu.org/?q=aliceholic&f=0&c=2_0");
+    var tableRows = doc.QuerySelectorAll("tr");
+
+    foreach (var tr in tableRows)
+    {
+        var titleElement = tr.QuerySelector("tr td:nth-child(2) a:nth-last-child(1)");
+        // 跳过表头
+        if (titleElement == null) continue;
+        var title = titleElement!.TextContent.Trim();
+        var artist = "";
+        // 使用正则表达式查找所有[]内的内容
+        MatchCollection items = Regex.Matches(title, @"\[([^\[\]]*)\]");
+        if (items.Count > 1)
+        {
+            Console.WriteLine($"{title}");
+            // 获取第一个匹配项
+            // title = items[0].Value;
+            // title = title.Substring(1, title.Length - 2);
+            artist = items[1].Value;
+            artist = artist.Substring(1, artist.Length - 2);
+        }
+        else if (items.Count == 1)
+        {
+            artist = items[0].Value;
+            artist = artist.Substring(1, artist.Length - 2);
+        }
+    
+        var link = titleElement!.GetAttribute("href")!.Trim();
+
+        var view = await WebParse.GetDoc("https://skb-nyaa.hacgn.eu.org" + link);
+        var desc = view.QuerySelector("#torrent-description")!.InnerHtml;
+
+        string imageUrl = "";
+        // 使用正则表达式查找第一个图片的URL  
+        string pattern = @"!\[[^\]]*\]\(([^)]*)\)";
+        Match match = Regex.Match(desc, pattern);
+        if (match.Success)
+        {
+            // match.Groups[1] 包含了第一个捕获组的内容，即图片的URL  
+            imageUrl = match.Groups[1].Value;
+        }
+
+        var magnetElement = tr.QuerySelector("tr td:nth-child(3) a:nth-child(2)");
+        var magnet = magnetElement!.GetAttribute("href")!.Trim();
+
+        artworks.Add(new Artwork(title, artist, magnet, imageUrl));
+        Console.WriteLine($"{title} {magnet}");
+    }
+}
 
 [JsonSerializable(typeof(List<Artwork>))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
